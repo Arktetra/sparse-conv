74ba6eccf7bdae453589daff837237e682308b34
Arktetra <sameershrestha.sm@gmail.com>
2026-01-14 22:09:16 +0545

    add kernel to insert 3d index as value

--- src/sparseconv/kernels/cuda/hash/hash.cu
+++ src/sparseconv/kernels/cuda/hash/hash.cu
@@ -300,3 +300,70 @@ torch::Tensor hashmap_lookup_3d_cuda(
 
     return output;
 }
+
+template<typename K, typename V>
+static __global__ void hashmap_insert_3d_idx_as_val_cuda_kernel(
+    const size_t N,
+    const size_t M,
+    int W, int H, int D,
+    K* __restrict__ hashmap_keys,
+    V* __restrict__ hashmap_values,
+    const int32_t* __restrict__ keys
+) {
+    const size_t thread_id = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (thread_id < M) {
+        int4 coord = reinterpret_cast<const int4*>(keys)[thread_id];
+        int b = coord.x;
+        int x = coord.y;
+        int y = coord.z;
+        int z = coord.w;
+        size_t flat_idx = (size_t)b * W * H * D + (size_t)x * H * D + (size_t)y * D + z;
+        K key = static_cast<K>(flat_idx);
+        V value = static_cast<V>(thread_id);
+        linear_probing_insert(hashmap_keys, hashmap_values, key, value, N);
+    }
+}
+
+template<typename K, typename V>
+void dispatch_hashmap_insert_3d_idx_as_val_cuda(
+    torch::Tensor& hashmap_keys,
+    torch::Tensor& hashmap_values,
+    const torch::Tensor& keys,
+    int W, int H, int D
+) {
+    hashmap_insert_3d_idx_as_val_cuda_kernel<<<
+        (keys.size(0) + BLOCK_SIZE - 1) / BLOCK_SIZE,
+        BLOCK_SIZE
+    >>>(
+        hashmap_keys.size(0),
+        keys.size(0),
+        W, H, D,
+        hashmap_keys.data_ptr<K>(),
+        hashmap_values.data_ptr<V>(),
+        keys.data_ptr<int32_t>()
+    );
+}
+
+void hashmap_insert_3d_idx_as_val_cuda(
+    torch::Tensor& hashmap_keys,
+    torch::Tensor& hashmap_values,
+    const torch::Tensor& keys,
+    int W, int H, int D
+) {
+    TORCH_CHECK(hashmap_keys.is_cuda(), "Hashmap keys must be on GPU.");
+    TORCH_CHECK(hashmap_values.is_cuda(), "Hashmap values must be on GPU.");
+    TORCH_CHECK(keys.is_cuda(), "Keys must be on GPU.");
+
+    if (hashmap_keys.dtype() == torch::kUInt32 && hashmap_values.dtype() == torch::kUInt32) {
+        dispatch_hashmap_insert_3d_idx_as_val_cuda<uint32_t, uint32_t>(hashmap_keys, hashmap_values, keys, W, H, D);
+    } else if (hashmap_keys.dtype() == torch::kUInt32 && hashmap_values.dtype() == torch::kUInt64) {
+        dispatch_hashmap_insert_3d_idx_as_val_cuda<uint32_t, uint64_t>(hashmap_keys, hashmap_values, keys, W, H, D);
+    } else if (hashmap_keys.dtype() == torch::kUInt64 && hashmap_values.dtype() == torch::kUInt32) {
+        dispatch_hashmap_insert_3d_idx_as_val_cuda<uint64_t, uint32_t>(hashmap_keys, hashmap_values, keys, W, H, D);
+    } else if (hashmap_keys.dtype() == torch::kUInt64 && hashmap_values.dtype() == torch::kUInt64) {
+        dispatch_hashmap_insert_3d_idx_as_val_cuda<uint64_t, uint64_t>(hashmap_keys, hashmap_values, keys, W, H, D);
+    } else {
+        TORCH_CHECK(false, "Unsupported data type");
+    }
+}
