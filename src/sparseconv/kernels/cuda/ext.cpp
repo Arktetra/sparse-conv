aa9883a1ec634e9e39d4c325a64859b775df135a
Arktetra <sameershrestha.sm@gmail.com>
2026-01-15 20:47:28 +0545

    create python bindings for neighbor map

--- src/sparseconv/kernels/cuda/ext.cpp
+++ src/sparseconv/kernels/cuda/ext.cpp
@@ -1,7 +1,9 @@
 #include <torch/extension.h>
 #include "hash/api.h"
+#include "neighbor_map/api.h"
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.def("hashmap_insert_cuda", &hashmap_insert_cuda);
     m.def("hashmap_lookup_cuda", &hashmap_lookup_cuda);
+    m.def("hashmap_build_submanifold_conv_neighbor_map_cuda_naive", &hashmap_build_submanifold_conv_neighbor_map_cuda_naive);
 }
